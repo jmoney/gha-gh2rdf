23be17c2228bef63f9a3f6cde21d46b0adf5c4bf
Jonathan Monette <jon.monette@gmail.com>
2023-08-23 08:30:05 -0500

    guard against none merged_at

--- main.py
+++ main.py
@@ -89,8 +89,9 @@ def pull_requests(g: rdflib.Graph, owner: str, org: str):
                 g.add((iri, GITHUB_NS.state, rdflib.Literal(pull.state)))
                 g.add((iri, GITHUB_NS.state_reason, rdflib.Literal(pull.state_reason)))
                 g.add((iri, GITHUB_NS.merged, rdflib.Literal(pull.pull_request.merged_at is not None, datatype=rdflib.XSD.boolean)))
-                g.add((iri, GITHUB_NS.merged_at, rdflib.Literal(pull.pull_request.merged_at, datatype=rdflib.XSD.dateTime)))
-                g.add((iri, GITHUB_NS.merged_on, rdflib.Literal(iso_merged_at.date(), datatype=rdflib.XSD.date)))
+                if pull.pull_request.merged_at is not None:
+                    g.add((iri, GITHUB_NS.merged_at, rdflib.Literal(pull.pull_request.merged_at, datatype=rdflib.XSD.dateTime)))
+                    g.add((iri, GITHUB_NS.merged_on, rdflib.Literal(iso_merged_at.date(), datatype=rdflib.XSD.date)))
                 for assignee in pull.assignees:
                     g.add((iri, GITHUB_NS.assignee, rdflib.Literal(assignee.login)))
                 g.add((iri, GITHUB_NS.created_at, rdflib.Literal(pull.created_at, datatype=rdflib.XSD.dateTime)))
